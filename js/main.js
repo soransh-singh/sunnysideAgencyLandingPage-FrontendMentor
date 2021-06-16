f535a1080323bb1f0ff28387670c72d9ce446a21
soransh <57911107+soransh-singh@users.noreply.github.com>
2021-06-16 12:59:06 +0530

    update css
    added navigation bar and make it working

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 window.addEventListener("load", ()=>{
   console.log("window loaded");
+
+  const nav = document.querySelector(".nav");
+  const navBtn = document.querySelector(".nav-btn");
+  console.log(nav, navBtn);
+  navBtn.addEventListener("click", ()=>{
+    nav.classList.toggle("nav-open")
+  })
 })
